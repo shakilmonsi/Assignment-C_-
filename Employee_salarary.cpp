589500ec2e8711e00ac417afa8d65cc78a2b95d6
shakilmonsi <samdshakilofficial@gmail.com>
2024-12-24 06:48:57 -0800

    see this docoument

--- Employee_salarary.cpp
+++ Employee_salarary.cpp
@@ -1,3 +1,14 @@
+// Design a class named Book with the following private attributes:
+// • title (string): Represents the title of the book.
+// • author (string): Represents the author of the book.
+// • ISBN (string): Represents the ISBN number of the book.
+// • price (double): Represents the price of the book.
+
+// Write a constructor that initializes these attributes. Create a method named
+// applyDiscount() that reduces the price by a given percentage. Add a public method
+// displayBookInfo() that displays the book's information, including the discounted price
+// if any.
+
 #include <iostream>
 #include <string>
 using namespace std;
