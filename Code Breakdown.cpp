e25f60837489a42f340bfa5c2e65517822d28735
shakilmonsi <samdshakilofficial@gmail.com>
2024-12-25 07:35:55 -0800

    number

---
+++ Code Breakdown.cpp
@@ -0,0 +1,13 @@
+// Trace the below given code according to the driver code and generate the final output
+// after executing the main function.
+// #include <iostream>
+// using namespace std;
+
+// int addNumbers(int a, int b) {
+// return a + b;
+// }
+// int main() {
+// int result = addNumbers(5, 10);
+// cout << "Sum: " << result << endl;
+// return 0;
+// }
